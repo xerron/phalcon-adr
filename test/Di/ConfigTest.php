d88f90836508c4155f58b360675f70e508cd743f
Edwin Manuel Cerrón Angeles <xerron.angels@gmail.com>
2017-05-26 19:55:09 -0500

    RC1 fix docheader

--- test/Di/ConfigTest.php
+++ test/Di/ConfigTest.php
@@ -2,7 +2,7 @@
 
 /**
  * @see       https://github.com/xerron/phalcon-expressive for the canonical source repository
- * @copyright Copyright (c) 2016-2017 U-w-U Digital Marketing Perú Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2016-2017 U-w-U Digital Marketing Perú Inc. (http://www.u-w-u.com)
  * @license   https://github.com/xerron/phalcon-expressive/blob/master/LICENSE.md New BSD License
  */
 namespace PhalconTest\Di;
